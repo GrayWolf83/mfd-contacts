b3ea1ded7bd82837c59022c5af0865ad3ec0ae8e
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2023-05-06 18:20:19 +0500

    Выравнивание размеров карточек групп и контактов

--- src/pages/GroupListPage.tsx
+++ src/pages/GroupListPage.tsx
@@ -1,16 +1,21 @@
-import React, {memo} from 'react';
-import {CommonPageProps} from './types';
-import {Col, Row} from 'react-bootstrap';
-import {GroupContactsCard} from 'src/components/GroupContactsCard';
+import React, { memo } from 'react'
+import { CommonPageProps } from './types'
+import { Col, Row } from 'react-bootstrap'
+import { GroupContactsCard } from 'src/components/GroupContactsCard'
 
-export const GroupListPage = memo<CommonPageProps>(({contactsState, groupContactsState}) => {
-  return (
-    <Row xxl={4}>
-      {groupContactsState[0].map((groupContacts) => (
-        <Col key={groupContacts.id}>
-          <GroupContactsCard groupContacts={groupContacts} withLink />
-        </Col>
-      ))}
-    </Row>
-  );
-});
+export const GroupListPage = memo<CommonPageProps>(
+	({ contactsState, groupContactsState }) => {
+		return (
+			<Row xs={1} md={2} lg={4} className='g-3'>
+				{groupContactsState[0].map((groupContacts) => (
+					<Col key={groupContacts.id} className='d-flex'>
+						<GroupContactsCard
+							groupContacts={groupContacts}
+							withLink
+						/>
+					</Col>
+				))}
+			</Row>
+		)
+	},
+)
